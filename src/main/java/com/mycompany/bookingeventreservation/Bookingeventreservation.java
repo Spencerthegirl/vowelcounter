7227c93aac9894d5b6ca745d25b6fadab0439cf8
RC_Student_lab <RC_Student_lab@10.0.0.238>
2024-10-23 09:17:20 +0000

    vowel count

---
+++ src/main/java/com/mycompany/bookingeventreservation/Bookingeventreservation.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bookingeventreservation;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Bookingeventreservation {
+
+    public static void main(String[] args) {
+                // Method to check password complexity
+    
+        
+                // Example usage of the reservation system
+        String bookingCode = "Event_123";
+        String password = "Passw0rd!";
+
+        String result = makeReservation(bookingCode, password);
+        System.out.println(result);  // Output will depend on the validity of
+
+    }
+}
